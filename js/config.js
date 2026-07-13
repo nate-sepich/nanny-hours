1e303525a217b1a0880bd1b11ce6565fc8a4de37
Nate Sepich <sepichnathan@gmail.com>
2026-07-13 15:47:42 -0500

    Point site at v3 Apps Script deployment

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Paste the Web App URL you get from deploying apps-script/Code.gs here.
 // See README.md for step-by-step setup instructions.
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwTjGtGMDsMhqGfMNB05NFwhaZ1wWdWnAgtVgdAHeZGgmQYP5dsutQOWLmB7Jupfb9c/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxin3LGQwjzV0m0DhtiaeRDMZvBgTpZpCmGliqknDlu1XPY_OJ84DQl19fxdioGYmYn/exec';
